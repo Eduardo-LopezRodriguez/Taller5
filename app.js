a13a90238f7e316e6fa15ceaf25b68c713f7c176
andrea <moralesandrea.edr4@gmail.com>
2020-08-26 10:40:17 -0600

    add ejs templates

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-// const https = require("https"); //this is to make requests to others servers
+const ejs = require("ejs");
+// const https = require("https"); 
+//this is to make requests to others servers
 // const { STATUS_CODES } = require("http");
 const app = express();
 
+app.use("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("public"));
 
