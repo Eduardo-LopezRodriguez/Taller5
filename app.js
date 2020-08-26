11c6b68ebbee0db7347adb1da9ba574315d86c7f
andrea <moralesandrea.edr4@gmail.com>
2020-08-26 11:11:08 -0600

    add date and fix typo line 7

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-// const https = require("https"); 
-//this is to make requests to others servers
-// const { STATUS_CODES } = require("http");
+
 const app = express();
 
-app.use("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    var day = "";
+    switch (currentDay) {
+
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+        case 7:
+            day = "Sunday"
+            break;
+    }
+    res.send("<h1>Today is " + day + "</h1>")
+});
+
 
 app.listen(3000, function() {
     console.log("Everything ok")
-});
-
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
 });
