26579ce7c5802bb98315861163c496606fcce2ce
andrea <moralesandrea.edr4@gmail.com>
2020-08-26 10:23:27 -0600

    fix errors

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// const https = require("https"); //this is to make requests to others servers
+// const { STATUS_CODES } = require("http");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+
+app.listen(3000, function() {
+    console.log("Everything ok")
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
